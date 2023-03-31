e487a481e469cb4a7877109165badffdce7a266b
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-03-31 10:43:59 +0200

    refactor: refactor codes of the calculator app
    Refactor codes and change some structure of the project in order to calculatore app works correctly.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bmicalculator'
+    'calculator',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bmicalculator.urls')),
+    path('', include('calculator.urls')),
 ]
