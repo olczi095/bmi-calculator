4bfd01a03c17927c0455e93108c6f19a2766d3d4
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-02 16:18:53 +0200

    feat: style and set up form of bmi calculator

--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from .forms import UserDataForm
 
 # Create your views here.
 def home(request):
@@ -6,4 +8,11 @@ def home(request):
     return redirect('bmi')
 
 def bmi_calculator(request):
-    return render(request, 'calculator/bmi.html')
+
+    if request.method == 'POST':
+            form = UserDataForm(request.POST)
+            if form.is_valid():  
+                return HttpResponseRedirect('thank you!')
+    else:
+        form = UserDataForm
+    return render(request, 'calculator/bmi.html', {'form': form})
