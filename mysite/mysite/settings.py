6e224d505cc27cfa3d897ea286cb443e2d3446e7
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-03 10:38:39 +0200

    fix: hide secret key and change DEBUG mode

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,24 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+*cd4mx)w8)4zl0)e@ru1o(u8in7(p=9503)8-85xq#v371&ki'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
