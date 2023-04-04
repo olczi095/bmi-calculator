eedb99cf8c892b38613bb6b3591f322ebed95f71
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-04 13:30:12 +0200

    feat: remove gender field from BMI form

--- mysite/calculator/forms.py
+++ mysite/calculator/forms.py
@@ -1,11 +1,7 @@
 from django import forms
 
 
-class UserDataForm(forms.Form):
-    gender = forms.ChoiceField(choices=[
-        ("male", "♂︎ Male"),
-        ("female", "♀︎ Female")
-    ])                     
+class UserDataForm(forms.Form):                
     age = forms.IntegerField(min_value=1, max_value=150)
     weight = forms.FloatField(min_value=1)
     height = forms.FloatField(min_value=1)
