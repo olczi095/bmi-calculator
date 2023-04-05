9455c0f944f3877c5a6b2423d4de739f20a55889
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-05 22:17:45 +0200

    feat: convert bmi_calculator function to BmiFormView class-based view

--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from .forms import UserDataForm
+from django.views.generic import FormView
 
-# Create your views here.
 def home(request):
     # Redirect directly to the bmi calculator website
     return redirect('bmi')
 
-def bmi_calculator(request):
 
-    if request.method == 'POST':
-            form = UserDataForm(request.POST)
-            if form.is_valid():  
-                return HttpResponseRedirect('thank you!')
-    else:
-        form = UserDataForm
-    return render(request, 'calculator/bmi.html', {'form': form})
+class BmiFormView(FormView):
+     form_class = UserDataForm
+     template_name = 'calculator/bmi.html'
