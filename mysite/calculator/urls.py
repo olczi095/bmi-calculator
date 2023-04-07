72e3259045ea3b656dc82d93d99e894c68ff22d1
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-07 17:10:08 +0200

    refactor: come back to function-based view

--- mysite/calculator/urls.py
+++ mysite/calculator/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import BmiFormView
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('bmi_calculator/', BmiFormView.as_view(), name='bmi'),
+    path('bmi_calculator/', views.bmi_calculator , name='bmi'),
 ]
--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import UserDataForm
 from django.views.generic import FormView
+from django.urls import reverse_lazy
 
 def home(request):
     # Redirect directly to the bmi calculator website
     return redirect('bmi')
 
-
-class BmiFormView(FormView):
-     form_class = UserDataForm
-     template_name = 'calculator/bmi.html'
+def bmi_calculator(request):
+    if request.method == 'POST':
+            form = UserDataForm(request.POST)
+            if form.is_valid():  
+                pass
+    else:
+        form = UserDataForm
+    return render(request, 'calculator/bmi.html', {'form': form})
