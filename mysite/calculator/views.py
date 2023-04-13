bb7a8b11b4363a48ee5312cb2f541629d0a66b73
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-13 10:47:48 +0200

    feat: create view with 'bmiresult.html' to display BMI score & category

--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
 from .forms import UserDataForm
-from django.views.generic import FormView
-from django.urls import reverse_lazy
-
+    
 def home(request):
-    # Redirect directly to the bmi calculator website
-    return redirect('bmi')
+   return redirect('bmi')
 
 def bmi_calculator(request):
     if request.method == 'POST':
             form = UserDataForm(request.POST)
-            if form.is_valid():  
-                pass
+            if form.is_valid():
+                height = form.cleaned_data['height']
+                weight = form.cleaned_data['weight']
+                calculated_bmi = round(weight / (height * 0.01) ** 2, 2)
+                calculated_bmi_category = checking_bmi_category(calculated_bmi)
+                return render(request, 'calculator/bmiresult.html', {'calculated_bmi': calculated_bmi,
+                                                                     'category': calculated_bmi_category[0],
+                                                                     'description': calculated_bmi_category[1]})
     else:
-        form = UserDataForm
-    return render(request, 'calculator/bmi.html', {'form': form})
+        form = UserDataForm()
+        return render(request, 'calculator/bmi.html', {'form': form})
+    
+def checking_bmi_category(bmi):
+    if bmi < 16:
+        return ('severe thinness', 'Your weight is too low and your life may be at risk. Try to'
+                'gain weight but if you may have problems with an eating disorder, please contant an experienced specialist.')
+    elif bmi < 18.5:
+        return ('underweight', 'Your weight is too low. You may be suffering from deficiency of multiple nutrient elements.'
+                'Try to gain weight or meet with a dietitian to receive a specific menu included your calorie needs.')
+    elif 18.5 <= bmi < 25:
+        return ('normal weight', 'Your weight is perfect! Congratulations and keep going. Still try to eat healthy'
+                ' and exercise at least three times a week for maintaining your weight and good health.')
+    elif 25 <= bmi < 30:
+        return ('overweight', 'Your weight is slightly too high and indicates a risk of obese. For improving your body appearance and health'
+                ' you should lose a few kilograms by changing a diet and trying to exercises more than before. Simple things can make a huge difference.')
+    elif 30 <= bmi < 35:
+        return ('obese', 'You have first degree obesity. It is a diseases that should be treated. At first you can try to do it on your own by changing diet, eating less calories and exercising more'
+                'but if you notices that your efforts do not yield expected results, contact with an experienced specialist like a dietitian or personal trainer.')
+    elif 35 <= bmi < 40:
+        return ('severely obese', 'You have second degree obesity. It is a serious danger for your health. Start treating as soon as it is possible.'
+                'Contact with a dietitian who has an experience in working with patients with obese and changed your whole lifestyle.')
+    else:
+        return ('morbidly obese', 'You have third degree obesity. This is a really danger diseases that threatens your health and even your life.'
+                'You should meet an experienced specialist immediately. Sometimes in the treatment it becomes necessary to involve psychologists, especially eating disorder therapists.')
