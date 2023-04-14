00b244d343810413a9c44efe20ac056417e34ab9
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-14 13:24:13 +0200

    feat: create placeholders for weight and height input fields

--- mysite/calculator/forms.py
+++ mysite/calculator/forms.py
@@ -3,5 +3,11 @@ from django import forms
 
 class UserDataForm(forms.Form):                
     age = forms.IntegerField(min_value=1, max_value=150)
-    weight = forms.FloatField(min_value=1, max_value=600)
-    height = forms.FloatField(min_value=1, max_value=300)
+    weight = forms.FloatField(min_value=1, max_value=600,
+                              widget=forms.NumberInput(attrs={'placeholder': 'kg ', 
+                                                              'style': 'text-align: right'
+                                                              }))
+    height = forms.FloatField(min_value=1, max_value=300, 
+                              widget=forms.NumberInput(attrs={'placeholder': 'cm ',
+                                                              'style': 'text-align: right'
+                                                              }))
