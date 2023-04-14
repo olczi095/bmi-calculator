e2c3074a4dbb71ed2e128fa9ad387a4f6427d4df
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-14 12:34:47 +0200

    feat: add limits for weight and height inputs in UserDataForm

--- mysite/calculator/forms.py
+++ mysite/calculator/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class UserDataForm(forms.Form):                
     age = forms.IntegerField(min_value=1, max_value=150)
-    weight = forms.FloatField(min_value=1)
-    height = forms.FloatField(min_value=1)
+    weight = forms.FloatField(min_value=1, max_value=600)
+    height = forms.FloatField(min_value=1, max_value=300)
