e423c8a1f568840471725a24b1d023fe67a6a728
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-16 11:35:27 +0200

    feat: modify UserDataForm and change selected fields in BMI calculator

--- mysite/calculator/forms.py
+++ mysite/calculator/forms.py
@@ -2,12 +2,15 @@ from django import forms
 
 
 class UserDataForm(forms.Form):                
-    age = forms.IntegerField(min_value=1, max_value=150)
-    weight = forms.FloatField(min_value=1, max_value=600,
+    age = forms.IntegerField(label='Age ', min_value=1, max_value=150)
+    weight = forms.FloatField(label='Weight    ', min_value=1, max_value=600,
                               widget=forms.NumberInput(attrs={'placeholder': 'kg ', 
-                                                              'style': 'text-align: right'
+                                                              'style': 'text-align: right; max-width: 70%; '
                                                               }))
-    height = forms.FloatField(min_value=1, max_value=300, 
+    height = forms.FloatField(label='Height ', min_value=1, max_value=300, 
                               widget=forms.NumberInput(attrs={'placeholder': 'cm ',
-                                                              'style': 'text-align: right'
+                                                              'style': 'text-align: right; max-width: 70%'
                                                               }))
+    gender = forms.ChoiceField(label='Gender ', choices = [
+                                ('male', 'male'),
+                                ('female', 'female')])
