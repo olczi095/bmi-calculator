f7e6aa952e5d5a55b3f866d39ae78668968e005a
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-04-19 12:42:06 +0200

    feat: add 'pal' ChoiceField field to 'UserDataForm'

--- mysite/calculator/forms.py
+++ mysite/calculator/forms.py
@@ -14,3 +14,14 @@ class UserDataForm(forms.Form):
     gender = forms.ChoiceField(required=False, label='Gender ', choices = [
                                 ('male', 'male'),
                                 ('female', 'female')])
+    pal = forms.ChoiceField(required=False, label='PAL ', choices = [
+                                ('1', '1.2'),
+                                ('2', '1.3'),
+                                ('3', '1.4'),
+                                ('4', '1.5'),
+                                ('5', '1.6'),
+                                ('6', '1.7'),
+                                ('7', '1.8'),
+                                ('8', '1.9'),
+                                ('9', '2.0'),
+                                ('10', '2.2')])
