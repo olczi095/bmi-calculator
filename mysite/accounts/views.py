bf067be1e5f73fcd40be6a2fc09e64dfb5033199
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-05-04 12:31:45 +0200

    feat: create 'Log in' view with validation for the user

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from calculator.views import home as home_page
 
 def signup_view(request):
     if request.method == 'POST':
@@ -10,3 +12,13 @@ def signup_view(request):
         form = UserCreationForm()
         return render(request, 'registration/signup.html', {'form': form})
     
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect(home_page)
+    else:
+        form = AuthenticationForm()
+        return render(request, 'registration/login.html', {'form': form})
--- mysite/calculator/urls.py
+++ mysite/calculator/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('bmi_calculator/', views.bmi_calculator , name='bmi'),
