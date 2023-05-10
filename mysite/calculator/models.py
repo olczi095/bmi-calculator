973326681cffad707ed8f3c0d253b24a55ad6358
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-05-10 11:48:15 +0200

    feat(accounts): create a LoggedInUser model

--- mysite/calculator/models.py
+++ mysite/calculator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class LoggedInUser(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+        )
+    weight = models.IntegerField()
+    height = models.IntegerField()
+    gender = models.CharField(
+        max_length=100
+        )
+    pal = models.CharField(
+        max_length=100
+        )
