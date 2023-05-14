6cc83bc0559e63cfdc3bbca72bf21c34ece6be5a
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-05-14 15:18:26 +0200

    feat(calculator): add Person and CalculatedData models

--- mysite/calculator/models.py
+++ mysite/calculator/models.py
@@ -3,10 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Person(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     weight = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
     gender = models.CharField(
@@ -14,12 +11,13 @@ class Person(models.Model):
         max_length=100
         )
     
+    def __str__(self):
+        return self.user.get_username()
+    
+
 
 class CalculatedData(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bmi = models.FloatField(default=0)
     bmi_category = models.CharField(
         default='unknown',
@@ -30,3 +28,6 @@ class CalculatedData(models.Model):
         max_length=100
         )
     tmr = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user.get_username()
