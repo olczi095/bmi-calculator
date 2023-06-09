0900bbdca7cb41063dab2d94997e2a778dad07f7
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-06-09 09:08:17 +0200

    test: add test of str representation for Person model

--- mysite/calculator/tests/test_models.py
+++ mysite/calculator/tests/test_models.py
@@ -9,46 +9,53 @@ class PersonTestCase(TestCase):
         self.user = User.objects.create_user(username='test_user', password='test_password')
         Person.objects.create(user=self.user, weight=80, height=175, gender='male', age=30)
 
+    def test_person_string_representation(self):
+        self.second_user = User.objects.create_user(username='test_second_user', password='test_second_password')
+        Person.objects.create(user=self.second_user, weight=60, height=160, gender='female', age=40)
+        test_person = Person.objects.get(id=2)
+        self.assertEqual(str(test_person), 'test_second_user')
+        self.assertTrue(isinstance(test_person, Person))
+
     def test_weight_label(self):
-        person = Person.objects.get(id=1)
-        weight_field_label = person._meta.get_field('weight').verbose_name
+        test_person = Person.objects.get(id=1)
+        weight_field_label = test_person._meta.get_field('weight').verbose_name
         self.assertEqual(weight_field_label, 'weight')
 
     def test_weight_value(self):
-        person = Person.objects.get(id=1)
-        person_weight = person.weight
+        test_person = Person.objects.get(id=1)
+        person_weight = test_person.weight
         expected_weight = 80
         self.assertEqual(person_weight, expected_weight)
 
     def test_height_label(self):
-        person = Person.objects.get(id=1)
-        height_field_label = person._meta.get_field('height').verbose_name
+        test_person = Person.objects.get(id=1)
+        height_field_label = test_person._meta.get_field('height').verbose_name
         self.assertEqual(height_field_label, 'height')
 
     def test_height_value(self):
-        person = Person.objects.get(id=1)
-        person_height = person.height
+        test_person = Person.objects.get(id=1)
+        person_height = test_person.height
         expected_height = 175
         self.assertEqual(person_height, expected_height)
 
     def test_gender_label(self):
-        person = Person.objects.get(id=1)
-        gender_field_label = person._meta.get_field('gender').verbose_name
+        test_person = Person.objects.get(id=1)
+        gender_field_label = test_person._meta.get_field('gender').verbose_name
         self.assertEqual(gender_field_label, 'gender')
 
     def test_gender_value(self):
-        person = Person.objects.get(id=1)
-        person_gender = person.gender
+        test_person = Person.objects.get(id=1)
+        person_gender = test_person.gender
         expected_gender = 'male'
         self.assertEqual(person_gender, expected_gender)
 
     def test_age_label(self):
-        person = Person.objects.get(id=1)
-        age_field_label = person._meta.get_field('age').verbose_name
+        test_person = Person.objects.get(id=1)
+        age_field_label = test_person._meta.get_field('age').verbose_name
         self.assertEqual(age_field_label, 'age')
 
     def test_age_value(self):
-        person = Person.objects.get(id=1)
-        person_age = person.age
+        test_person = Person.objects.get(id=1)
+        person_age = test_person.age
         expected_value = 30
         self.assertEqual(person_age, expected_value)
