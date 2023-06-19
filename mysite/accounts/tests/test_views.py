c69312cd99c4b6df8dff50c2691e2a00ef21ebfd
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-06-19 11:55:56 +0200

    test(accounts): add tests of sign up

---
+++ mysite/accounts/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class SignUpTestCase(TestCase):
+
+    valid_data = {'username': 'monika123',
+                  'email': 'monika@mail.com', 
+                  'password1': 'mojeHaslo1.',
+                  'password2': 'mojeHaslo1.'}
+                  
+    def test_display_registration_view(self):
+        response = self.client.get(reverse('accounts:signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+        
+        
+    def test_redirect_after_valid_sign_up(self):
+        response = self.client.post(reverse('accounts:signup'), self.valid_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'calculator/bmi.html')
+        user = User.objects.get(username='monika123')
+        self.assertTrue(user.is_authenticated)
