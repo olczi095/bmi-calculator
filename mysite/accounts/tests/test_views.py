430a7513f0efcf61ea9fcf2984e8ce2376baef17
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-06-20 13:23:58 +0200

    refactor(accounts): change values of data in test_views

--- mysite/accounts/tests/test_views.py
+++ mysite/accounts/tests/test_views.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 
 class SignUpTestCase(TestCase):
 
-    valid_data = {'username': 'monika123',
-                  'email': 'monika@mail.com', 
-                  'password1': 'mojeHaslo1.',
-                  'password2': 'mojeHaslo1.'}
+    valid_data = {'username': 'testuser',
+                  'email': 'testuser@test.com', 
+                  'password1': 'testpassword',
+                  'password2': 'testpassword'}
                   
     def test_display_registration_view(self):
         response = self.client.get(reverse('accounts:signup'))
@@ -20,15 +20,15 @@ class SignUpTestCase(TestCase):
         response = self.client.post(reverse('accounts:signup'), self.valid_data, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'calculator/bmi.html')
-        user = User.objects.get(username='monika123')
+        user = User.objects.get(username='testuser')
         self.assertTrue(user.is_authenticated)
 
 
     def test_signup_view_post_invalid_form(self):
         response = self.client.post(reverse('accounts:signup'), {'username': 'testuser', 
                                                                  'email': 'testuser@mail.com',
-                                                                 'password1': 'correctpass', 
-                                                                 'password2': 'notcorrectpas'})
+                                                                 'password1': 'testpassword', 
+                                                                 'password2': 'invalidtestpassword'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/signup.html')
         self.assertFalse(response.context['form'].is_valid())
