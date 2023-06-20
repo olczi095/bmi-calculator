24b1b42c9f663f7661830d99a833339f357d7eca
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-06-20 10:02:08 +0200

    test(accounts): complete SignUpTestCase

--- mysite/accounts/tests/test_views.py
+++ mysite/accounts/tests/test_views.py
@@ -22,3 +22,13 @@ class SignUpTestCase(TestCase):
         self.assertTemplateUsed(response, 'calculator/bmi.html')
         user = User.objects.get(username='monika123')
         self.assertTrue(user.is_authenticated)
+
+
+    def test_signup_view_post_invalid_form(self):
+        response = self.client.post(reverse('accounts:signup'), {'username': 'testuser', 
+                                                                 'email': 'testuser@mail.com',
+                                                                 'password1': 'correctpass', 
+                                                                 'password2': 'notcorrectpas'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+        self.assertFalse(response.context['form'].is_valid())
