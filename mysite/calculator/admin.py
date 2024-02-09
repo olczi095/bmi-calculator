d565db770a997848e3fe2cfe43413ac4a1ae82b0
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2024-02-09 11:22:47 +0100

    feat: set readonly fields in admin
    The readonly fields were necessary for Person and CalculatedData models.

--- mysite/calculator/admin.py
+++ mysite/calculator/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Person, CalculatedData
 
-admin.site.register(Person)
-admin.site.register(CalculatedData)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    readonly_fields = ('user',)
+
+
+@admin.register(CalculatedData)
+class CalculatedDataAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', 'bmi', 'bmi_category', 'bmr', 'pal', 'tmr')
