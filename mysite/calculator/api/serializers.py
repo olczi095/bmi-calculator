c4a8ffe1162098f66125446622c007781f77d529
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2024-02-14 08:33:25 +0100

    feat(api): implement permissions for PersonViewSet

--- mysite/calculator/api/serializers.py
+++ mysite/calculator/api/serializers.py
@@ -4,8 +4,8 @@ from calculator.models import Person
 
 
 class PersonSerializer(serializers.ModelSerializer):
-    person_id = serializers.IntegerField(source='id')
-    user_id = serializers.IntegerField(source='user.id', required=False)
+    person_id = serializers.IntegerField(source='id', read_only=True)
+    user_id = serializers.IntegerField(source='user.id', required=False, read_only=True)
 
     class Meta:
         model = Person
--- mysite/calculator/api/views.py
+++ mysite/calculator/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAdminUser
 
 from calculator.models import Person
 
+from .permissions import IsOwnerOrIsAdminUser
 from .serializers import PersonSerializer
 
 
@@ -10,3 +11,8 @@ class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.exclude(user__is_active=False)
     serializer_class = PersonSerializer
     permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.action in ('retrieve', 'update', 'partial_update'):
+            self.permission_classes = [IsOwnerOrIsAdminUser]
+        return super().get_permissions()
