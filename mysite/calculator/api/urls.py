464288351d4cfb57e551723858dbc8105ecba7be
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2024-02-15 09:48:41 +0100

    feat(api): implement API authentication
    JWT tokens are now used for user authentication in the calculator API.

--- mysite/calculator/api/urls.py
+++ mysite/calculator/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import PersonViewSet
 
@@ -8,7 +9,8 @@ router.register('persons', PersonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'calculator',
     'accounts',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -109,3 +110,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
