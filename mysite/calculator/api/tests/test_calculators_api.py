6e39eae776704a4d3cc892cd67462a125594987e
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2024-02-20 11:35:59 +0100

    test: refactor BMICalculationAPI tests
    API Tests were updated to check serializres for input validation.

--- mysite/calculator/api/tests/test_calculators_api.py
+++ mysite/calculator/api/tests/test_calculators_api.py
@@ -6,28 +6,63 @@ from rest_framework.test import APITestCase
 class BMICalculationAPITest(APITestCase):
     def setUp(self):
         self.path = reverse('bmi-calculation')
+        self.data = {
+            "height": 170,
+            "weight": 70
+        }
 
     def test_get_method_not_allowed(self):
         response = self.client.get(self.path)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def test_post_method_valid_data(self):
-        data = {"height": 170, "weight": 70}
-        response = self.client.post(self.path, data=data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('bmi', response.data)
-        self.assertIn('bmi_category', response.data)
-        self.assertIn('description', response.data)
-
     def test_post_method_with_valid_data(self):
-        data = {"height": 170.0, "weight": 70.5}
-        response = self.client.post(self.path, data=data)
+        response = self.client.post(self.path, data=self.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_post_method_with_invalid_data(self):
-        data = {"height": -170, "weight": 70}
+        expected_fields = ['bmi', 'bmi_category', 'description', 'message']
+        for expected_field in expected_fields:
+            self.assertIn(expected_field, response.data)
+
+    def test_bmi_calculation(self):
+        expected_bmi = 24.22
+        expected_bmi_category = 'normal weight'
+
+        response = self.client.post(self.path, data=self.data)
+        self.assertEqual(response.data['bmi'], expected_bmi)
+        self.assertEqual(response.data['bmi_category'], expected_bmi_category)
+        self.assertEqual(response.data['message'], 'BMI calculated successfully')
+
+    # Tests with invalid data
+    def test_post_method_without_data(self):
+        data = {}
         response = self.client.post(self.path, data=data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        for field in response.data:
+            self.assertEqual('This field is required.', response.data[field][0])
+
+    def test_post_method_with_negative_height(self):
+        self.data['height'] = -170
+        response = self.client.post(self.path, data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual('Height must be greater than 0', response.data['height'][0])
+
+    def test_post_method_with_height_greater_than_300(self):
+        self.data['height'] = 310
+        response = self.client.post(self.path, data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual('Height must be less than 300', response.data['height'][0])
+
+    def test_post_method_with_negative_weight(self):
+        self.data['weight'] = -70
+        response = self.client.post(self.path, data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual('Weight must be greater than 0', response.data['weight'][0])
+
+    def test_post_method_with_weight_greater_than_1000(self):
+        self.data['weight'] = 1100
+        response = self.client.post(self.path, data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual('Weight must be less than 1000', response.data['weight'][0])
 
 
 class BMRCalculationAPITest(APITestCase):
