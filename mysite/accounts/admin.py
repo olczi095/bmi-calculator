199d36384448135aa4fc2c7f7fb5eafef1d126da
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2024-02-21 09:21:21 +0100

    feat: define a new User admin

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name",
+                    "last_name", "is_staff", "is_active")
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
